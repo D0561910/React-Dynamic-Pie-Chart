ce65825a1859e9e9e50adf3b3dbacc685d3b1a82
d0561910 <d0561910@mail.fcu.edu.tw>
2021-07-11 16:25:55 +0800

    Adding title and css style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Pie from "./components/Pie";
 function App() {
   return (
     <div className="App">
+      <div className="Header">Charles Dynamic Pie Chart</div>
       <Pie />
     </div>
   );
